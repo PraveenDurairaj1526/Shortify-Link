e76b9e3033af3508678cf462b1e6eb054f355991
PraveenDurairaj1526 <praveendurairaj.in@gmail.com>
2025-12-09 08:43:27 +0530

    link hold FUN addion

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -21,13 +21,29 @@ export default async function RedirectPage({ params }) {
             clickCount: increment(1)
         });
         if (data?.verification == 'not-approved') {
-            return (
-                <div className="flex flex-col items-center justify-center min-h-screen">
-                      <h2 className="text-3xl font-semibold mb-2 text-red-500">Link Blocked</h2>
-                    <p>This link has been blocked by the admin because it violates our content guidelines. Please review our Privacy Policy for more details.</p>
-                </div>
-            )
-        } else {
+            if (data?.linkHold) {
+                return (
+                    <div className="flex flex-col items-center justify-center min-h-screen">
+                        <h2 className="text-3xl font-semibold mb-2 text-yellow-700">Link On Hold</h2>
+                        <p>
+                            The admin couldn’t verify this link, so it has been temporarily placed on hold.
+                            Please contact the admin for assistance.
+                        </p>
+                        <a href="mailto:praveendurairaj.in@gmail.com?subject=Link%20Verification%20Request" className="mt-4 bg-blue-600 text-white px-5 py-2 rounded-lg">
+                            Contact Admin
+                        </a>
+                    </div>
+                )
+            }
+            else {
+                return (
+                    <div className="flex flex-col items-center justify-center min-h-screen">
+                        <h2 className="text-3xl font-semibold mb-2 text-red-500">Link Blocked</h2>
+                        <p>This link has been blocked by the admin because it violates our content guidelines. Please review our Privacy Policy for more details.</p>
+                    </div>)
+            }
+        }
+        else {
             redirect(data.originalUrl);
         }
     } else {
